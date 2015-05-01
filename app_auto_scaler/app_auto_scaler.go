12ce106f18ea882ac7871602a59ee1f05250086a
Sabha Parameswaran <sparameswaran@gopivotal.com>
2015-05-01 16:05:13 -0700

    Add github package name

--- app_auto_scaler/app_auto_scaler.go
+++ app_auto_scaler/app_auto_scaler.go
@@ -18,25 +18,10 @@ import (
 	"sync"
 	//"encoding/json"
 	//"cloudfoundry.com/cf-utils"
-	cfutils "cf_utils"
-	scalerutils "scaler_utils"
+	cfutils "github.com/cf-platform-eng/autoscaler/cf_utils"	
+	scalerutils "github.com/cf-platform-eng/autoscaler/scaler_utils"
 )
 
-/*
-type AppDetail struct {
-  TargetQ string `json:"queue"`
-  AppName string `json:"app"`
-  Org     string `json:"org"`
-  Space   string `json:"space"`
-}
-
-type AppInstanceDetail struct {
-  TargetQ   string
-  AppName   string
-  AppGuid   string
-  Instances int
-}
-*/
 
 var api_endpoint string
 
